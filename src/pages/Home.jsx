6a36bf453216976ad48adac2f55ce94b0ed78aff
Niten Kumawat <nitenkumawat@gmail.com>
2024-12-20 18:52:50 +0530

    home page scroll problem solution

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,19 @@ import PostSection from '../components/PostSection';
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <div className="bg-gray-100 flex lg:-ml-10 md:-ml-8 sm:-ml-0  justify-center w-full ">
-        <div className="w-full max-w-screen-xl h-full flex  ">
+    <div className='sticky top-0 bg-white z-10'>
+    <Navbar />
+    </div>
+      
+      <div className="bg-gray-100 flex lg:-ml-10 md:-ml-8 sm:-ml-0 justify-center w-full">
+        <div className="w-full max-w-screen-xl h-full flex">
           {/* Left Column - Filter Section */}
-          <div className=" lg:flex flex-col w-1/4 p-2 ">
+          <div className="lg:flex flex-col w-1/4  sticky top-16 h-fit">
             <Filter />
           </div>
 
           {/* Middle Column - Search and Posts Section */}
-          <div className="flex-1 ">
+          <div className="flex-1 h-full ">
             <div className="mb-3">
               <SearchQA />
             </div>
@@ -25,7 +28,7 @@ const Home = () => {
           </div>
 
           {/* Right Column - Optional Component */}
-          <div className="lg:flex flex-col w-1/4 bg-white p-4  mx-2 mt-3 shadow-md rounded">
+          <div className="lg:flex flex-col w-1/4 bg-white h-fit p-4 mx-2 mt-3 shadow-md rounded sticky top-16">
             <div>
               <h2 className="text-lg font-semibold">Right Column</h2>
               <p className="text-gray-600">Content for the right column goes here.</p>
